3e23eb1832f264919c25f91c48575e768184f97f
Abhinav Kumar <abhinavkumarctps786@gmail.com>
2024-11-29 13:08:16 +0530

    index file changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Import from 'react-dom/client'
-import App from "./App.js";
+import App from "./App";
 import "./index.css"; // Importing your global styles (if any)
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // Create the root
